2b33c138c6a883a17541c848ab7beeea6fae5022
sadique-cws <sadique.hussain96@gmail.com>
2022-02-11 09:05:42 +0530

    hello calling

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ var app = express()
 var bodyParser = require('body-parser')
 var flash = require('express-flash');
 var usersRouter = require('./routers')
+var session = require('express-session')
+const mongoose = require('./connect')
 
 // pug configs
 app.set("views","./views")
 app.set("view engine","pug")
 
+
+
+app.use(session({
+    secret:'1234567catr',
+    resave: false,
+    saveUninitialized:true,
+    cookie:{maxAge:60000}
+}))
+
+app.use(flash())
+
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(flash())
+
 app.use("/",usersRouter)
 
 
--- routers.js
+++ routers.js
@@ -1,22 +1,21 @@
+const { Route } = require('express');
 var express = require('express')
 var router = express.Router();
 
-var mongoose = require('./connect');
 var userModel = require('./userModel');
 
-router.get("/",function(req,res){
+router.get("/insert",function(req,res){
     res.render("home")
 })
 
-router.post("/signup",function(req,res, next){
-    req.assert('name', 'name is required').notEmpty();
-    req.assert('email', "a valid email is required").isEmail();
-    req.assert('contact', "contact is required").notEmpty();
-    req.assert('password', "password is required").notEmpty();
+router.get("/",(req,res)=>{
+    userModel.find({},function(error,users){
+        res.render("data",{"users": users});
+    })
+})
 
-    var errors = req.validationErrors();
-
-    if(!errors){
+router.post("/signup",function(req,res){
+   
         var userDetails = new userModel({
             name: req.body.name,
             email: req.body.email,
@@ -24,25 +23,9 @@ router.post("/signup",function(req,res, next){
             password : req.body.password
         });
 
-        userDetails.save((err, doc) => {
-                if(!err){
-                    req.flash('success',"user data inserted successfully")
-                    req.redirect("/");
-                }
-                else{
-                    console.log("error during record insertion")
-                }
-        })
-    }
-    else{
-        var error_msg = '';
-        errors.forEach(function(error){
-            error_msg += error.msg + "<br>"
-        })
-        req.flash('error',error_msg)
-
-        res.redirect("/")
-    }
+        userDetails.save()
+        req.flash('success',"user data inserted successfully")
+        return res.redirect("/")
 })
 
 module.exports = router;
